9b959f8815c06d65c834fca05923346bcfa8adfb
Felipe Andrade dos Santos <andrade.adsf@gmail.com>
2022-09-10 18:00:05 -0300

    strlcat done

---
+++ ft_strlcat.c
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strlcat.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: feandrad <feandrad@student.42sp.org.br>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/09/10 14:38:23 by feandrad          #+#    #+#             */
+/*   Updated: 2022/09/10 14:38:23 by feandrad         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stddef.h>
+#include <libft.h>
+
+size_t	ft_strlcat(char *dst, const char *src, size_t size)
+{
+	size_t	count_dst;
+	size_t	count_src;
+
+	count_dst = ft_strlen(dst);
+	count_src = 0;
+	while (src[count_src] != '\0' && count_src < (size - 1))
+	{
+		dst[count_dst] = src[count_src];
+		count_src++;
+		count_dst++;
+	}
+	dst[count_dst] = '\0';
+	return (ft_strlen(dst));
+}
