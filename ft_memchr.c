155840f145942e636a06f8bc838aa03eeaf364d7
feandrad <feandrad@student.42sp.org.br>
2022-09-14 23:53:34 +0200

    memchr done

---
+++ ft_memchr.c
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memchr.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: feandrad <feandrad@student.42sp.org.br>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/09/14 23:42:51 by feandrad          #+#    #+#             */
+/*   Updated: 2022/09/14 23:52:39 by feandrad         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void	*memchr(const void *s, int c, size_t n)
+{
+	size_t	count;
+
+	count = 0;
+	while (count < n && s[count] != '\0')
+	{
+		if (s[count] == c)
+			return ((char *)&s[count]);
+		else
+			count++;
+	}
+	return (NULL);
+}
