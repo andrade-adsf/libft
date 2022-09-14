b59ce50f8072db4c939ef86c23513cfedd8ac84e
feandrad <feandrad@student.42sp.org.br>
2022-09-15 01:47:12 +0200

    ft_memcmp done

---
+++ ft_memcmp.c
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memcmp.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: feandrad <feandrad@student.42sp.org.br>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/09/14 23:54:05 by feandrad          #+#    #+#             */
+/*   Updated: 2022/09/15 00:06:41 by feandrad         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+int	memcmp(const void *s1, const void *s2, size_t n)
+{
+	int		count;
+	char	*str1;
+	char	*str2;
+
+	count = 0;
+	str1 = (char *)s1;
+	str2 = (char *)s2;
+	while ((str1[count] == str2[count]) && (str1[count] != '\0')
+		&& (str2[count] != '\0') && (count < n))
+		count++;
+	return (s1[count] - s2[count]);
+}
