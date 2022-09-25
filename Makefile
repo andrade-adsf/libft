127853477540bed35726b024962616336bf7b480
feandrad <feandrad@student.42sp.org.br>
2022-09-25 22:56:20 +0200

    updated with all files

--- Makefile
+++ Makefile
@@ -2,19 +2,18 @@ NAME = libft.a
 FLAGS = -Wall -Wextra -Werror
 REMOVE = rm -rf
 
-#		Parte 1
 SRCS = 	ft_atoi.c ft_bzero.c ft_calloc.c \
 		ft_isalnum.c ft_isalpha.c ft_isascii.c \
-		ft_isdigit.c ft_isprint.c ft_memchr.c \
-		ft_memcmp.c ft_memcpy.c ft_memmove.c \
-		ft_memset.c ft_strchr.c ft_strdup \
-		ft_strlcat.c ft_strlcpy.c ft_strlen.c \
+		ft_isdigit.c ft_isprint.c ft_itoa.c \
+		ft_memchr.c ft_memcmp.c ft_memcpy.c \
+		ft_memmove.c ft_memset.c ft_putchar_fd.c \
+		ft_putendl_fd.c ft_putnbr_fd.c ft putstr_fd.c \
+		ft_split.c ft_strchr.c ft_strdup.c \
+		ft_striteri.c ft_strjoin.c ft_strlcat.c \
+		ft_strlcpy.c ft_strlen.c ft_strmapi.c \
 		ft_strncmp.c ft_strnstr.c ft_strrchr.c \
-		ft_tolower.c ft_toupper.c \
-		# Parte 2
-		ft_substr ft_strjoin ft_strtrim \
-		ft_putchar_fd ft_putstr_fd ft_putendl_fd \
-		ft_putnbr_fd
+		ft_strtrim.c ft_substr.c ft_tolower.c \
+		ft_toupper.c
 
 OBJS = $(SRCS:.c=.o)
 
--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: feandrad <feandrad@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/12 19:02:45 by feandrad          #+#    #+#             */
-/*   Updated: 2022/09/17 02:06:25 by feandrad         ###   ########.fr       */
+/*   Updated: 2022/09/25 22:32:27 by feandrad         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -45,6 +45,10 @@ char	ft_tolower(int c);
 char	*ft_strjoin(char const *s1, char const *s2);
 char	*ft_substr(char const *s, unsigned int start, size_t len);
 char	*ft_strtrim(char const *s1, char const *set);
+char	**ft_split(char const *s, char c);
+char	*ft_itoa(int n);
+char	*ft_strmapi(char const *s, char (*f)(unsigned int, char));
+void	ft_striteri(char *s, void (*f)(unsigned int, char*));
 void	ft_putchar_fd(char c, int fd);
 void	ft_putendl_fd(char *s, int fd);
 void	ft_putnbr_fd(int n, int fd);
